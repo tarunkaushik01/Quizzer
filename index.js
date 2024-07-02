6ee9168bfd37b475e4cf3cd35023187fb5a26e79
Tarun <90602855+tarunkaushik01@users.noreply.github.com>
2024-07-02 23:02:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const take = document.getElementById('Take');
+take.addEventListener('click',function(){
+    let x = prompt("Please Enter the access key");
+    if(x != ""){
+        alert("Sorry, but the work is in Progess");
+    }
+})
